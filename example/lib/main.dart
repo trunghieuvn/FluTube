e557e9afab7a2ff94128e6c45d9c8ec44fb47572
ja2375 <webmaster@javi-h.com>
2019-05-19 19:13:54 +0200

    Complete plugin rewrite start

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,62 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:flutube/flutube.dart';
 
-void main() => runApp(MaterialApp(
-  home: MyApp(),
-));
+void main() => runApp(
+  MaterialApp(
+    home: MyApp(),
+  )
+);
 
-class MyApp extends StatefulWidget {
-  @override
-  MyAppState createState() => MyAppState();
-}
-
-class MyAppState extends State<MyApp> {
-  final List<String> playlist = <String>[
-    'https://www.youtube.com/watch?v=fq4N0hgOWzU',
-    'https://www.youtube.com/watch?v=D-o4BqJxmJE',
-    'https://www.youtube.com/watch?v=hA0hrpR-o8U',
-    'https://www.youtube.com/watch?v=RS36gBEp8OI',
-  ];
-  int currentPos;
-  String stateText;
-
-  @override
-  void initState() {
-    currentPos = 0;
-    stateText = "Video not started";
-    super.initState();
-  }
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('FluTube Test'),
+        title: Text('FluTube Example'),
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: <Widget>[
-            Text('Youtube video URL: ${playlist[currentPos]}', style: TextStyle(fontSize: 16.0),),
-            FluTube.playlist(
-              playlist,
-              autoInitialize: true,
-              aspectRatio: 16 / 9,
-              allowMuting: false,
-              onVideoStart: () {
-                setState(() {
-                  stateText = 'Video started playing!';
-                });
-              },
-              onVideoEnd: () {
-                setState(() {
-                  stateText = 'Video ended playing!';
-                  if((currentPos + 1) < playlist.length)
-                    currentPos++;
-                });
-              },
-            ),
-            Text(stateText),
-          ],
+      body: Center(
+        child: FluTube(
+          'https://www.youtube.com/watch?v=fq4N0hgOWzU',
         ),
       ),
     );
--- lib/src/flutube_player.dart
+++ lib/src/flutube_player.dart
@@ -1,319 +1,61 @@
 import 'dart:async';
+import 'dart:convert';
+import 'package:flutter/services.dart';
 
 import 'package:flutter/material.dart';
-import 'package:chewie/chewie.dart';
-import 'package:flutter/services.dart';
-import 'package:http/http.dart' as http;
-import 'package:video_player/video_player.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class FluTube extends StatefulWidget {
-  /// Youtube video URL(s)
-  var _videourls;
+  /// USER INPUT VARS
 
-  /// Initialize the Video on Startup. This will prep the video for playback.
-  final bool autoInitialize;
+  /// YouTube video URL
+  final String videoUrl;
 
-  /// Play the video as soon as it's displayed
-  final bool autoPlay;
-
-  /// Start video at a certain position
-  final Duration startAt;
-
-  /// Whether or not the video should loop
-  final bool looping;
-
-  /// Whether or not to show the controls
-  final bool showControls;
-
-  /// The Aspect Ratio of the Video. Important to get the correct size of the
-  /// video!
-  ///
-  /// Will fallback to fitting within the space allowed.
+  /// Video aspect ratio
   final double aspectRatio;
 
-  /// Allow screen to sleep
-  final bool allowScreenSleep;
-
-  /// Show mute icon
-  final bool allowMuting;
-
-  /// Show fullscreen button.
-  final bool allowFullScreen;
-
-  /// Device orientation when leaving fullscreen.
-  final List<DeviceOrientation> deviceOrientationAnterFullscreen;
-
-  /// System overlays when exiting fullscreen.
-  final List<SystemUiOverlay> systemOverlaysAfterFullscreen;
-
-  /// The placeholder is displayed underneath the Video before it is initialized
-  /// or played.
-  final Widget placeholder;
-
-  /// Play video directly in fullscreen
-  final bool fullscreenByDefault;
-
-  /// Whether or not to show the video thumbnail when the video did not start playing.
-  final bool showThumb;
-
-  /// Video events
-
-  /// Video start
-  final VoidCallback onVideoStart;
-
-  /// Video end
-  final VoidCallback onVideoEnd;
-
+  /// Constructor
   FluTube(
-    @required String videourl, {
-    Key key,
-    this.aspectRatio,
-    this.autoInitialize = false,
-    this.autoPlay = false,
-    this.startAt,
-    this.looping = false,
-    this.placeholder,
-    this.showControls = true,
-    this.fullscreenByDefault = false,
-    this.showThumb = true,
-    this.allowMuting = true,
-    this.allowScreenSleep = false,
-    this.allowFullScreen = true,
-    this.deviceOrientationAnterFullscreen,
-    this.systemOverlaysAfterFullscreen,
-    this.onVideoStart,
-    this.onVideoEnd,
-  }) : super(key: key) {
-    this._videourls = videourl;
-  }
-
-  FluTube.playlist(
-    @required List<String> playlist, {
-    Key key,
-    this.aspectRatio,
-    this.autoInitialize = false,
-    this.autoPlay = false,
-    this.startAt,
-    this.placeholder,
-    this.looping,
-    this.showControls = true,
-    this.fullscreenByDefault = false,
-    this.showThumb = true,
-    this.allowMuting = true,
-    this.allowScreenSleep = false,
-    this.allowFullScreen = true,
-    this.deviceOrientationAnterFullscreen,
-    this.systemOverlaysAfterFullscreen,
-    this.onVideoStart,
-    this.onVideoEnd,
-  }) : super(key: key) {
-    assert(playlist.length > 0, 'Playlist should not be empty!');
-    this._videourls = playlist;
-  }
+      this.videoUrl, {
+      this.aspectRatio = 16 / 9, // Aspect ratio defaults to 16 / 9
+  });
 
   @override
-  FluTubeState createState() => FluTubeState();
+  _FluTubeState createState() => _FluTubeState();
 }
 
-class FluTubeState extends State<FluTube>{
-  VideoPlayerController videoController;
-  ChewieController chewieController;
-  bool isPlaying = false;
-  bool _needsShowThumb;
-  List<String> _playlistUrls; // Internal list of initializaed video urls of a playlist
-  int _currentlyPlaying = 0; // Track position of currently playing video
-
-  bool get _isPlaylist => widget._videourls is List<String>;
-
-  @override
-  initState() {
-    super.initState();
-    _needsShowThumb = !widget.autoPlay;
-    if(_isPlaylist) {
-      _playlistUrls = List();
-      _initialize((widget._videourls as List<String>)[0]); // Play the very first video of the playlist
-
-      _initializeAllVideos();
-    } else {
-      _initialize(widget._videourls as String);
-    }
-  }
-
-  _initializeAllVideos() async {
-    for(String url in widget._videourls as List<String>) {
-      _playlistUrls.add(await _fetchVideoURL(url));
-    }
-  }
-
-  void _initialize(String _url) {
-    _fetchVideoURL(_url).then((url) {
-      videoController = VideoPlayerController.network(url)
-        ..addListener(_playingListener)
-        ..addListener(_endListener);
-
-      // Video start callback
-      if(widget.onVideoStart != null) {
-        videoController.addListener(_startListener);
-      }
-
-      chewieController = ChewieController(
-          videoPlayerController: videoController,
-          aspectRatio: widget.aspectRatio,
-          autoInitialize: widget.autoInitialize,
-          autoPlay: widget.autoPlay,
-          startAt: widget.startAt,
-          looping: widget.looping,
-          placeholder: widget.placeholder,
-          showControls: widget.showControls,
-          fullScreenByDefault: widget.fullscreenByDefault,
-          allowFullScreen: widget.allowFullScreen,
-          deviceOrientationsAfterFullScreen: widget.deviceOrientationAnterFullscreen,
-          systemOverlaysAfterFullScreen: widget.systemOverlaysAfterFullscreen,
-          allowedScreenSleep: widget.allowScreenSleep,
-          allowMuting: widget.allowMuting
-      );
-    });
-  }
-
-  _playingListener() {
-    if(isPlaying != videoController.value.isPlaying){
-      setState(() {
-        isPlaying = videoController.value.isPlaying;
-      });
-    }
-  }
-
-  _startListener() {
-    if(videoController.value.initialized && isPlaying)
-      widget.onVideoStart();
-  }
-
-  _endListener() {
-    // Video end callback
-    if(videoController.value.initialized && !widget.looping){
-      if(videoController.value.position >= videoController.value.duration){
-        if(isPlaying){
-          chewieController.pause();
-          chewieController.seekTo(Duration());
-        }
-        if(widget.onVideoEnd != null)
-          widget.onVideoEnd();
-        if(widget.showThumb && !_isPlaylist){
-          setState(() {
-            _needsShowThumb = true;
-          });
-        }
-        if(_isPlaylist && _currentlyPlaying < _playlistUrls.length - 1){
-          _playlistLoadNext();
-        }
-      }
-    }
-  }
-
-  _playlistLoadNext() {
-    chewieController.dispose();
-    setState(() {
-      _currentlyPlaying++;
-    });
-    videoController.pause();
-    videoController = null;
-    videoController = VideoPlayerController.network(_playlistUrls[_currentlyPlaying]);
-    videoController.addListener(_playingListener);
-    videoController.addListener(_endListener);
-    chewieController = ChewieController(
-        videoPlayerController: videoController,
-        aspectRatio: widget.aspectRatio,
-        autoInitialize: true,
-        showControls: widget.showControls,
-        fullScreenByDefault: widget.fullscreenByDefault,
-        allowFullScreen: widget.allowFullScreen,
-        deviceOrientationsAfterFullScreen: widget.deviceOrientationAnterFullscreen,
-        systemOverlaysAfterFullScreen: widget.systemOverlaysAfterFullscreen,
-        allowedScreenSleep: widget.allowScreenSleep,
-        allowMuting: widget.allowMuting
-    );
-    chewieController.play();
-  }
-
-  @override
-  void dispose() {
-    videoController.dispose();
-    chewieController.dispose();
-    super.dispose();
-  }
+class _FluTubeState extends State<FluTube> {
+  WebViewController _controller;
 
   @override
   Widget build(BuildContext context) {
-    print(_currentlyPlaying);
-    if(widget.showThumb && !isPlaying && _needsShowThumb){
-      return Center(
-        child: Container(
-          width: MediaQuery.of(context).size.width,
-          child: AspectRatio(
-            aspectRatio: widget.aspectRatio,
-            child: Stack(
-              fit: StackFit.expand,
-              children: <Widget>[
-                Image.network(
-                  _videoThumbURL(_isPlaylist ? widget._videourls[_currentlyPlaying] : widget._videourls),
-                  fit: BoxFit.cover,
-                ),
-                Center(
-                  child: ClipOval(
-                    child: Container(
-                      color: Colors.white,
-                      child: IconButton(
-                        iconSize: 50.0,
-                        color: Colors.black,
-                        icon: Icon(
-                          Icons.play_arrow,
-                        ),
-                        onPressed: () {
-                          setState(() {
-                            videoController.play();
-                            _needsShowThumb = false;
-                          });
-                        },
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ),
-      );
-    } else {
-      return chewieController != null ? Chewie(
-        key: widget.key,
-        controller: chewieController,
-      ) : AspectRatio(
-        aspectRatio: widget.aspectRatio,
-        child: Center(
-          child: CircularProgressIndicator(),
-        ),
-      );
-    }
+    _loadHtmlFromAssets();
+    return AspectRatio(
+      aspectRatio: widget.aspectRatio,
+      child: Builder(
+        builder: (BuildContext context) {
+          return WebView(
+            initialUrl: Uri.dataFromString('assets/yt_player.html', mimeType: 'text/html').toString(),
+            javascriptMode: JavascriptMode.unrestricted,
+            onWebViewCreated: (WebViewController webViewController) {
+              _controller = webViewController;
+            },
+            onPageFinished: (url) {
+              print(url);
+              _controller.evaluateJavascript('loadVideo("${widget.videoUrl.substring(widget.videoUrl.indexOf('v=') + 2)}", 0);');
+            },
+          );
+        }
+      ),
+    );
   }
 
-  Future<String> _fetchVideoURL(String yt) async {
-    final response = await http.get(yt);
-    Iterable parseAll = _allStringMatches(response.body, RegExp("\"url_encoded_fmt_stream_map\":\"([^\"]*)\""));
-    final Iterable<String> parse = _allStringMatches(parseAll.toList()[0], RegExp("url=(.*)"));
-    final List<String> urls = parse.toList()[0].split('url=');
-    parseAll = _allStringMatches(urls[1], RegExp("([^&,]*)[&,]"));
-    String finalUrl = Uri.decodeFull(parseAll.toList()[0]);
-    if(finalUrl.indexOf('\\u00') > -1)
-      finalUrl = finalUrl.substring(0, finalUrl.indexOf('\\u00'));
-    return finalUrl;
-  }
-
-  Iterable<String> _allStringMatches(String text, RegExp regExp) => regExp.allMatches(text).map((m) => m.group(0));
-
-  String _videoThumbURL(String yt) {
-    String id = yt.substring(yt.indexOf('v=') + 2);
-    if(id.contains('&'))
-      id = id.substring(0, id.indexOf('&'));
-    return "http://img.youtube.com/vi/$id/0.jpg";
+  _loadHtmlFromAssets() async {
+    String fileText = await rootBundle.loadString('assets/yt_player.html');
+    _controller.loadUrl( Uri.dataFromString(
+        fileText,
+        mimeType: 'text/html',
+        encoding: Encoding.getByName('utf-8')
+    ).toString());
   }
 }
